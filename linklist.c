2a8cca4eab0c4877eb25985ab4cbdb5b1f54b99e
vaishnavgowda <54174794+vaishnavgowda@users.noreply.github.com>
2019-09-30 15:40:38 +0530

    Update linklist.c

--- linklist.c
+++ linklist.c
@@ -1,95 +1,232 @@
-#include<stdlib.h>
-#include<stdio.h>
+#include <stdio.h>
+#include <stdlib.h>
 
-typedef struct node
-{
-  int data;
-  struct node *next;
-}*Node;
+struct node{
+	int val;
+	struct node *next;
+	};
+	
+typedef struct node* node;
 
-Node getnode()
+node get_node();
+node ins_f(node first, int val);
+node ins_b(node first, int val);
+node ins_any_position(node first , int val , int pos);
+node del_front(node first);
+node del_back(node first);
+node del_any_pos(node first , int pos);
+void display(node first);
+
+node first = NULL;
+
+int main(){
+	int i, x, val,pos;
+	
+	while(x != 8){
+		printf("\n1: Enter val from front\n");
+		printf("2: Enter val from back\n");
+                printf("3: Enter val at any given position\n");
+                printf("4: Delete from front\n");
+                printf("5: Delete from back\n"); 
+                printf("6: Delete at any given position\n");
+		printf("7: Display\n");
+		printf("8: Exit\n");
+		scanf("%d", &x);
+			switch(x){
+			case 1:
+				printf("\nEnter value from front: \n");
+				scanf("%d", &val);
+				first = ins_f(first, val);
+				break;
+				
+			case 2:
+				printf("\nEnter value from back: \n");
+				scanf("%d", &val);
+				first = ins_b(first, val);
+				break;
+
+                        case 3: printf("\nEnter the value to be inserted and position\n");
+                                scanf("%d%d",&val,&pos);
+                                first = ins_any_position(first , val , pos);
+                                break;
+                   
+                        case 4: first = del_front(first);
+                                break;
+   
+                        case 5: first = del_back(first);
+                                break;
+ 
+                        case 6: printf("\nEnter the position to be deleted\n");
+                                scanf("%d",&pos);
+                                first = del_any_pos(first , pos); 
+                                break;
+				
+			case 7: printf("\nThe elements in the list are:\n");
+				display(first);
+				break;
+                                
+			}
+		}
+	}
+		
+node get_node(){
+	node temp;
+	temp = (node)malloc(sizeof(struct node));
+	temp->next = NULL;
+	return temp;
+	}
+	
+	
+
+node ins_f(node first, int val){
+	node temp;
+	if(first == NULL){
+		first = get_node();
+		first-> val = val;
+		}
+	else{
+		temp = get_node();
+		temp->val = val;
+		temp->next = first;
+		first = temp;
+		
+		}
+	return first;	
+	}
+
+node ins_b(node first, int val){
+	node temp, curr;
+	if(first == NULL){
+		first = get_node();
+		first-> val = val;
+		}
+	else{
+		curr  = first;
+		while(curr->next != NULL){
+			curr = curr->next;
+			}
+				 
+		temp = get_node();
+		temp->val = val;
+		curr->next = temp;
+		}
+	return first;
+	}
+
+node ins_any_position(node first , int val , int pos)
 {
-  Node p;
-  p=(Node) malloc (sizeof(Node));
-  if(p!=NULL)
-    return p;
+  int count = 1;
+  node temp,cur,prev;
+  temp = get_node();
+  temp->val = val;
+  temp->next = NULL;
+  if(first == NULL && pos == 1)
+  {
+    return temp;
+  }
+  if(first == NULL && pos!=1)
+  {
+    printf("Invalid position");
+  }
+  cur = first;
+  prev = NULL;
+  while(cur->next != NULL && count != pos)
+  {
+    prev = cur;
+    cur = cur->next;
+    count++;
+  }
+  if(count == pos)
+  {
+    prev->next = temp;
+    temp->next = cur;
+    return first;
+  }
   else
-    {
-      printf("Memory is not allocated");
-      exit(0);
-    }
+  {
+    printf("\nNot a valid position\n");
+  }
+} 
+
+node del_front(node first)
+{
+  node temp;
+  if(first == NULL)
+  {
+    printf("\nList is empty\n");
+  }
+  temp = first;
+  printf("\nDeleted element is %d",temp->val);
+  temp = temp->next;
+  free(first);
+  first = temp;
+  return first;
 }
 
-Node insert_front(Node head,int item)
+node del_back(node first)
 {
-  Node p;
-  p=getnode();
-  p->data=item;
-  p->next=head;
-  head=p;
-  return head;
+  node cur,prev;
+  if(first == NULL)
+  {
+    printf("\nList is empty\n");
+  }
+  cur = first;
+  prev = NULL;
+  while(cur->next != NULL)
+  {
+    prev = cur;
+    cur = cur->next;
+  }
+  prev->next = NULL;
+  free(cur);
+  return first;
 }
 
-Node insert_end(Node head,int item)
+node del_any_pos(node first , int pos)
 {
-  Node p,q;
-  q=getnode();
-  q->data=item;
-  q->next=NULL;
-  if(head==NULL)
-     return q;
-  p=head;
-  while(p->next!=NULL)
-     p=p->next;
-  p->next=q;
-  return head;
-}
-
-
-void display(Node head)
-{
-  Node p;
-  if(head==NULL)
-    {
-      printf("list is empty\n");
-    }
+  node cur,prev;
+  int count = 1;
+  if(first == NULL)
+  {
+    printf("\nList is empty\n");
+  }
+  if(first->next == NULL && pos == 1)
+  {
+    printf("\nThe deleted element is %d\n\n",first->val);
+    free(first);
+    return NULL;
+  }
+  if(first == NULL && pos!=1)
+  {
+    printf("\nInvalid position\n");
+  }
+  cur = first;
+  prev = NULL;
+  while(cur->next != NULL && count != pos)
+  {
+    prev = cur;
+    cur = cur->next;
+    count++;
+  }
+  if(count == pos)
+  {
+    prev->next = cur->next;
+    printf("\nThe deleted element is %d\n",cur->val);
+    free(cur);
+  }
   else
-    {
-      p=head;
-      printf("Element in the list: ");
-      while(p!=NULL)
-        {
-          printf("%d ",p->data);
-          p=p->next;
-         }
-      printf("\n");
-     }
+  {
+    printf("\nNot a valid position\n");
+  }
+  return first;
 }
-
-int main()
-{
-  Node head=NULL;
-  int option,ele,value,pos;
-  do
-   {
-     printf("\n1)Insert Front\n2)Insert End\n3)Display\n4)Exit\n");
-     printf("Enter your choice: "); 
-     scanf("%d",&option);
-     switch(option)
-       {
-         case 1: printf("Enter the element to be inserted: ");
-                 scanf("%d",&ele);
-                 head=insert_front(head,ele);
-                 break;
-         case 2: printf("Enter the element to be inserted: ");
-                 scanf("%d",&ele);
-                 head=insert_end(head,ele);
-                 break;
-         case 3: display(head);
-                 break;
-         case 4: break;
-         default: printf("Invalid choice\n");
-       }
-    }while(option!=5);
-  return 0;
-}
+  
+	
+void display(node first){
+	node curr;
+	curr = first;
+	while(curr != NULL){
+		printf("%d\n", curr->val);
+		curr = curr->next;
+		}
+	}
